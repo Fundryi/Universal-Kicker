a8d74440f5267b64b9f93bbcb4cc22ad105cc0be
Fundryi <fundryi@gmail.com>
2022-01-30 16:28:04 +0100

    Command change to "/kickrole" and now tells you when no one is left to kick.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import hikari
 import time
 from hikari.api.interaction_server import Response
+from hikari.undefined import count
 import lightbulb
 from hikari.guilds import Guild, Role
 from lightbulb.checks import has_role_permissions
@@ -26,17 +27,23 @@ async def on_start(event):
 
 @bot.command
 @lightbulb.option('role', 'All users that are in this group will be kicked!', type=Role)
-@lightbulb.command('kk', 'Kicks all users in a specific role.')
+@lightbulb.command('kickrole', 'Kicks all users in a specific role.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def MassKick(ctx):
+
+    kickedRole = ctx.options.role.mention
+
     for member in ctx.get_guild().get_members().values():
         if ctx.options.role in member.get_roles():
             await ctx.get_guild().kick(member, reason='The role you are in got cleaned from our server.')
 
             memberID = member.mention
-            kickedRole = ctx.options.role.mention
+            userRoleAmount = count(ctx.options.role in member.get_roles())
 
-            await ctx.respond(f'The User {memberID} is being kicked because he is in the role {kickedRole}!')
+            await ctx.respond(f'The User {memberID} is being kicked because he is in the role {kickedRole}! | {userRoleAmount} left to kick')
             time.sleep(2)
+        else:
+             await ctx.respond(f'No one left to kick in the {kickedRole} role!')  
+        break
 
 bot.run()
